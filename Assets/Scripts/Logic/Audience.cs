efa69ba73c2023afa7ce32f7a9b67fa3dfc9cf80
Mario Petricevic <Petricevic@adesso-mobile.de>
2023-09-23 16:06:39 +0200

    Adjusts table for standing position

--- Assets/Scripts/Logic/Audience.cs
+++ Assets/Scripts/Logic/Audience.cs
@@ -1,21 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace MacounDemo
 {
     public class Audience : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
     }
 }
